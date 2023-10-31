3199c2b70d84eb8652699a721a17a1f7a6bbacf0
arifmunandarjunior2023 <146052000+arifmunandarjunior2023@users.noreply.github.com>
2023-10-31 13:48:47 +0800

    KULIAH ALGO 31 OKTOBER 2023

---
+++ 00-TEMPLATE/src/App.java
@@ -0,0 +1,27 @@
+public class App {
+    public static void main(String[] args) throws Exception ;// PROGRAM BIODATA MAHASISWA
+    }
+String Nama;
+String Nim;
+String Tgl_Lahir;
+String Alamat;
+Float Berat_BadaN;
+Float Tinggi_Badan;
+
+// mengisi variable
+Nama = "Arif"
+Nim ="23241060"
+Tgl_Lahir="4 Januari 2005"
+Alamat="Hidirasa Wera Nusa Tenggara Barat"
+ Berat_Badan= ""
+Tinggi_Badan=""
+
+// cetak biodata 
+System.out.println("=============================================");
+Sysrem.out.println("Nama:"+ nama);
+System.out.println("NIM:"+nim);
+System.out.println("Tanggal Lahir:"+tgl_lahir);
+System.out.println("Alamat:"+alamat);
+System.out.println("Berat Badan"+berat_BadaN);
+System.out.println("Berat Badan"+berat_badan);
+System.out.println("Tinggi Badan"+Tinggi_Badan);
