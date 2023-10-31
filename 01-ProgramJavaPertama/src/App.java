08a0417524bcaeab5201183edafa41bbfb025bf0
arifmunandarjunior2023 <146052000+arifmunandarjunior2023@users.noreply.github.com>
2023-10-31 13:58:09 +0800

    Kuliah Algo 31 Oktober 2023

---
+++ 01-ProgramJavaPertama/src/App.java
@@ -0,0 +1,27 @@
+public class App {
+    public static void main(String[] args) throws Exception ;}
+    // PROGRAM BIODATA MAHASISWA
+    //DEKLARASI VARIABLE
+String Nama;
+String Nim;
+String Tgl_Lahir;
+String Alamat;
+Float Berat_Badan;
+Float Tinggi_Badan;
+
+// mengisi variable
+Nama = "Arif"
+Nim ="23241060"
+Tgl_Lahir = "4 Januari 2005"
+Alamat = "Hidirasa Wera Nusa Tenggara Barat"
+Berat_Badan = ""
+Tinggi_Badan = ""
+
+// cetak biodata 
+System.out.println("=============================================");
+Sysrem.out.println("Nama:"+ Nama);
+System.out.println("NIM:"+ Nim);
+System.out.println("Tanggal Lahir:"+ Tgl_Lahir);
+System.out.println("Alamat:"+ Alamat);
+System.out.println("Berat Badan"+ Berat_Badan);
+System.out.println("Tinggi Badan"+ Tinggi_Badan);
